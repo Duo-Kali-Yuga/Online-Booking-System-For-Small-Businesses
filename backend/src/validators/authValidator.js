b3804b0bbb0403b8893b5f6db843d7ea703e6a6d
Alberto A.S. <elberthwolfimap@gmail.com>
2026-04-21 21:22:10 +0800

    Fix some bugs

--- backend/src/validators/authValidator.js
+++ backend/src/validators/authValidator.js
@@ -4,7 +4,7 @@ export const registerSchema = z.object({
   name: z.string().min(2),
   email: z.string().email(),
   password: z.string().min(6),
-  role: z.enum(["admin", "client"]).optional(),
+  role: z.enum(["admin", "client", "provider"]).optional(),
 });
 
 export const loginSchema = z.object({
