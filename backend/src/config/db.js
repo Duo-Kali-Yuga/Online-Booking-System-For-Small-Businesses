f13a4ef8cf181b98ada35fd1cc4bfc264f046987
Alberto A.S. <elberthwolfimap@gmail.com>
2026-04-27 17:59:09 +0800

    Fixed Monngose, now Render Host

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ import "dotenv/config"
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGOOSE_URL);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("DB connection error:", error.message);
