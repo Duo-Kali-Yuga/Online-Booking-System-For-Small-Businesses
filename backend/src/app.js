68ec8214451d27a0f3ba5de10027cb742d26e98e
Alberto A.S. <elberthwolfimap@gmail.com>
2026-06-01 20:22:33 +0800

    Hello 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,8 +22,8 @@ const app = express();
 
 
 const allowedOrigins = [
-  "mongodb://localhost:27017/booking_system", // Local Version
-  // 'https://booking-businesses.vercel.app', // Internet 
+  //"mongodb://localhost:27017/booking_system", // Local Version
+  'https://booking-businesses.vercel.app', // Internet 
 ];
 
 app.use(cors({
