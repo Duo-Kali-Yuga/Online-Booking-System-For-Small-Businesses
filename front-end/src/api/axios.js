2421a781b6d389639a4e65673618ae3297a9f997
Alberto A.S. <elberthwolfimap@gmail.com>
2026-06-02 13:50:01 +0800

    fefabr

--- front-end/src/api/axios.js
+++ front-end/src/api/axios.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   // Use your local backend port
-  baseURL: import.meta.env.VITE_API_URL || 'https://bookingbusinesses.onrender.com/api',
+  //baseURL: import.meta.env.VITE_API_URL || 'https://bookingbusinesses.onrender.com/api',
 
   // Use your Internet backend port
-  // baseURL: "https://bookingbusinesses.onrender.com/api",
+    baseURL: "https://bookingbusinesses.onrender.com/api",
 });
 
 // Add a request interceptor to include the JWT token
